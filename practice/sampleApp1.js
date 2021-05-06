ad8c2e4f8e2a57bb03d29ab504ecb4622f3192e7
ChiYoPU <gossa192566@gmail.com>
2021-05-06 15:32:42 +0900

    新規ユーザー作成コンポーネントを実装

--- practice/sampleApp1.js
+++ practice/sampleApp1.js
@@ -130,12 +130,59 @@ ver router = new VueRouter({
     ]
 })
 
+/* 記事取得の為の擬似APIを取得 */
 ver postUser = function(params,callback){
     setTimeout(function(){
         params.id = userData.length + 1
         userData.push(params)
         callback(null,params)
     },1000)
+}
+
+/* 新規ユーザー作成コンポーネント */
+ver UserCreate = {
+    template:`#user-create`,
+    data:function(){
+        return{
+            sending:false,
+            user:this.defaultUser(),
+            error:null
+        }
+    },
+    created:function(){
+
+    },
+    methods:{
+        defaultUser:function(){
+            return{
+                name:"",
+                description:""
+            }
+        },
+        createUser:function(){
+            if(this.user.name.trim() === ""){
+                this.error = `Nameは必須です`
+                return
+            }
+            if(this.user.description.trim() === ""){
+                this.error = `Descriptionは必須です`
+                return
+            }
+            postUser(this.user, (function(err,user){
+                this.sending = false
+                if(err){
+                    this.error = err.toString()
+                }else{
+                    this.error = null
+                    //デフォルトでフォームをリセット
+                    this.user = this.defaultUser()
+                    alert(`新規ユーザーが登録されました`)
+                    //ユーザー一覧ページに戻る
+                    this.$route.push(`/users`)
+                }
+            }).bind(this))
+        }
+    }
 }
 
 ver app = new Vue({
