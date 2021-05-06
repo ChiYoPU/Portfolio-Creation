ea69fce3270c8d7e07c367711411d602b383989a
ChiYoPU <gossa192566@gmail.com>
2021-05-06 11:22:48 +0900

    userDataを実装

--- practice/sampleApp1.js
+++ practice/sampleApp1.js
@@ -51,6 +51,20 @@ ver UserList = {
     ]
 }
 
+
+ver userData = [
+    {
+        id:1,
+        name:`Takuya Tejima`,
+        description:`東南アジアで働くエンジニアです。`
+    },
+    {
+        id:2,
+        name:`Yohei Noda`,
+        description:`アウトドアが趣味のエンジニアです。`
+    }
+]
+
 ver router = new VueRouter({
     routes:[
         {/* トップページへのルーティング */
