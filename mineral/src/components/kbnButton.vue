92ab9101a8ed060812b39c0309125a01bf43facd
ChiYoPU <gossa192566@gmail.com>
2021-08-28 17:29:02 +0900

    ボタンコンポーネントを作成

---
+++ mineral/src/components/kbnButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <button type="button">{{ button }}</button>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data(){
+        return{
+            button: "Sign in"
+        }
+    }
+}
+</script>
+
+
+<style lang="scss">
+button{
+    font-size: 25px;
+    font-weight: bold;
+    color: #F7FBFC;
+    background-color: #3F72AF;
+    border-radius: 15%;
+}
+</style>
