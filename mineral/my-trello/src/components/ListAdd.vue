457ae7090e2a00f88c475ed1a0eed73804cc3b4b
ChiYoPU <gossa192566@gmail.com>
2022-06-14 22:49:12 +0900

    フォームの活性、非活性でスタイルの切り替えを実装

--- mineral/my-trello/src/components/ListAdd.vue
+++ mineral/my-trello/src/components/ListAdd.vue
@@ -1,9 +1,11 @@
 <template>
-    <form class="addlist" @submit.prevent="addlist">
+    <form :class="classlist" @submit.prevent="addlist">
         <input v-model="title"
                 type="text"
                 class="text-input"
                 placeholder="Add new list"
+                @focusin="startEditing"
+                @focusout="finishEditing"
         >
         <button type="submit" class="add-button">
             Add
@@ -16,12 +18,28 @@ export default {
     data: function(){
         return {
             title: '',
+            isEditing: false,
+        }
+    },
+    computed: {
+        classList(){
+            const classList = ['addlist']
+            if(this.isEditing){
+                classList.push('active')
+            }
+            return classList
         }
     },
     methods: {
         addList: function(){
             this.$store.dispatch('addlist',{title: this.title})
             this.title = ''
+        },
+        startEditing: function(){
+            this.isEditing = true
+        },
+        finishEditing: function(){
+            this.isEditing = false
         },
     }
 }
