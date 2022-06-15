5fd22d6bc9cd6081452dde6551923ee74111dff0
ChiYoPU <gossa192566@gmail.com>
2022-06-15 10:16:11 +0900

    typoを修正

--- mineral/my-trello/src/store/index.js
+++ mineral/my-trello/src/store/index.js
@@ -3,49 +3,46 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-//LocalStorageに保存したリストの取得
 const savedLists = localStorage.getItem('trello-lists')
 
+
 const store = new Vuex.Store({
-  state: {
-    state: {
-      //保存したリストがあれば取得し、ないときに表示されるデフォルトを定義
-    lists: savedLists ? JSON.parse(savedLists): [
-        {
-          title: 'Backlog',
-          cards: [
-            { body: 'English' },
-            { body: 'Mathematics' },
-          ]
-        },
-        {
-          title: 'Todo',
-          cards: [
-            { body: 'Science' }
-          ]
-        },
-        {
-          title: 'Doing',
-          cards: []
-        }
-      ],
+state: {
+  lists: savedLists ? JSON.parse(savedLists): [
+      {
+        title: 'Backlog',
+        cards: [
+          { body: 'English' },
+          { body: 'Mathematics' },
+        ]
+      },
+      {
+        title: 'Todo',
+        cards: [
+          { body: 'Science' }
+        ]
+      },
+      {
+        title: 'Doing',
+        cards: []
+      }
+    ],
   },
   mutations: {
-    addlist(state, payload){
-      state.lists.push({title: payload.title, cards:[]})
+    addlist(state, payload) {
+      state.lists.push({ title: payload.title, cards:[] })
     },
   },
   actions: {
-    addlist(context, payload){
+    addlist(context, payload) {
       context.commit('addlist', payload)
-    }
+    },
   },
   getters: {
   }
-}})
+})
 
 store.subscribe((mutation, state) => {
-  //LocalStorageにデータの更新をして、Jsonに変換して保存
   localStorage.setItem('trello-lists', JSON.stringify(state.lists))
 })
 
